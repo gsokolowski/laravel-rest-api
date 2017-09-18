135b24d1396db1629186b7a5c9bbe2563276cb10
Greg <gregor.sokolowski@gmail.com>
2017-09-18 16:04:43 +0100

    Handling all types of exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,10 +4,15 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Traits\ApiResponser;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,16 +51,66 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // handling validation exceptions
         if ($exception instanceof ValidationException) {
             return $this->convertValidationExceptionToResponse($exception, $request);
         }
 
+        // handling model not found exceptions
         if ($exception instanceof ModelNotFoundException) {
             $modelName = class_basename($exception->getModel());
             return $this->errorResponse("Does not exist any {$modelName} with specified identyficator", 404);
         }
 
-        return parent::render($request, $exception);
+        // handling authentication exceptions
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        // handling authorisation exceptions
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), 403);
+        }
+
+        // handling not found http exception when url does not exist
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse("The specify url can't be found", 404);
+        }
+
+        // handling not found http exception when url does not exist
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse("The specify http method not allowed", 405);
+        }
+
+        // Handling General HttpException
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        // handling QueryException when you have foreighn keys to other table and trying to eg. delete seller or buyer
+        if ($exception instanceof QueryException) {
+            // see all info about that exception
+            //dd($exception)
+            $errorCode = $exception->errorInfo[1];
+            if ($errorCode == 1451) {
+                return $this->errorResponse("Cannot remove this (row) resource permanently. It is related to (other table) resource. You need to remove first relation", 409);
+            }
+        }
+
+        // Handling Unexpected Exceptions
+        // If all exeptions above are not triggerd that means db is down or server is down or anything of that matter
+        // so you need to handle this also by directly returning Unexpected Exceptions message as json for production
+        // If yo
+
+        // from .env
+        if(config('app.debug')) {
+            // display normal full info for development mode debug mode
+            return parent::render($request, $exception);
+        } else {
+            // this is for production
+            return $this->errorResponse("Unexpected Exceptions. Try later", 500);
+        }
+
     }
     /**
      * Convert an authentication exception into an unauthenticated response.
@@ -66,10 +121,7 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-        return redirect()->guest('login');
+        return $this->errorResponse('Unauthenticated', 401);
     }
     /**
      * Create a response object from the given validation exception.
