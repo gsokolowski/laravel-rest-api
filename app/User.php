5b73cbc5b449d54b1484e309d3c4501cac302b1e
Greg <gregor.sokolowski@gmail.com>
2017-09-19 12:23:39 +0100

    soft delete

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable
+    use Notifiable;
 
     // for sof deleting - build in laravel feature
     use SoftDeletes;
